02d4c096af66fc665130276d6c8efffacac0feb5
Victor Jimenez <vjimenez@vistaprint.com>
2019-02-27 16:58:07 +0100

    Add output containing the lambda names

--- modules/lambda/output.tf
+++ modules/lambda/output.tf
@@ -1,3 +1,7 @@
 output "lambda_arns" {
   value = "${zipmap(keys(var.functions), aws_lambda_function.lambda_function.*.arn)}"
 }
+
+output "lambda_names" {
+  value = "${zipmap(keys(var.functions), aws_lambda_function.lambda_function.*.function_name)}"
+}
